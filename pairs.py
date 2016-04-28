f496d269936fa1a19b7f7c1361e8461f73cca02f
Fred Bartle <fredbartle@gmail.com>
2016-04-27 23:20:54 -0400

    Simplify logic, smidge faster over a million iterations.

--- pairs.py
+++ pairs.py
@@ -7,26 +7,22 @@ SAMPLE_ARRAY = [1, 1, 2, 4, 4, 5, 5, 5, 6, 7, 9]
 
 def pairs(array):
     results = []
-    seen = []
 
-    for a in array:
-
+    # 1, 2, 3, 4
+    for a in range(1, 5):
         b = 10 - a
 
-        if a in seen or b in seen:
-            continue
+        if a in array and b in array:
+            # found at least one pair
+            a_count = array.count(a)
+            b_count = array.count(b)
+            results.extend([(a, b)] * a_count * b_count)  # forward
+            results.extend([(b, a)] * a_count * b_count)  # reverse
 
-        a_count = array.count(a)
-        b_count = array.count(b)
-
-        if b in array:
-            if a == b:  # 5's are special, removes pairing with self
-                results.extend([(a, b)] * a_count * (b_count - 1))
-            else:
-                results.extend([(a, b)] * a_count * b_count)  # forward
-                results.extend([(b, a)] * a_count * b_count)  # reverse
-
-        seen.append(a)
+    # 5's are special, ignore pairing with self
+    if 5 in array:
+        count = array.count(5)
+        results.extend([(5, 5)] * count * (count - 1))
 
     results1 = sorted(results)
     results2 = sorted(set(results1))  # remove duplicates
